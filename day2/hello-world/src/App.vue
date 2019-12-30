56262eb08e1802ab42c28de4f6adbd0a43d3505d
Harkin <1493297552@qq.com>
2019-12-30 08:52:13 +0800

    提交了day2

---
+++ day2/hello-world/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    我是home页面
+  </div>
+</template>
+
+<style lang="scss">
+html,body{
+  width: 100%;
+  height: 100%;
+}
+#app {
+  width: 100%;
+  height: 100%;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+// #nav {
+//   padding: 30px;
+
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
+
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
+</style>
